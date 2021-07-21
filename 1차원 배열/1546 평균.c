8b52f80deed9ab6aabfc1f0fc38b917787fecd79
mersombre <83867104+mersombre@users.noreply.github.com>
2021-07-21 23:47:17 +0900

    Create 1546 평균.c

---
+++ 1차원 배열/1546 평균.c
@@ -0,0 +1,31 @@
+////////// C //////////
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+ 
+    int n;
+    int scr[1000];
+    int max = scr[0];
+    double newscr[1000]; // 새로 정의를 해 줘야 함...
+    double sum = 0; // sum도 마찬가지..
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &scr[i]);
+        if (scr[i] > max) max = scr[i];
+    }
+    
+    for (int i = 0; i < n; i++) {
+        newscr[i] = ((double)scr[i]) / max * 100;
+				// scr[i]를 double로 형변환해줬기 때문에
+				// newscr[i] 자리에 그대로 scr[i]를 쓸 수 없다 => double형인 새 배열 만들어줌
+        sum += newscr[i];
+    }
+
+    printf("%.2f", sum/n);
+
+    return 0;
+}
