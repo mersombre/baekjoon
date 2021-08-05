2b94d9ce3f4a3bd8ccd6315820526869317abf7f
mersombre <83867104+mersombre@users.noreply.github.com>
2021-08-05 21:27:39 +0900

    Create 2752 세수정렬(1).py

---
+++ 2752 세수정렬(1).py
@@ -0,0 +1,8 @@
+a = list(map(int, input().split()))
+
+for i in range(len(a) - 1):
+    for j in range(len(a) - i - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print(a[0], a[1], a[2])
