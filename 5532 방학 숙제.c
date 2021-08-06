5a0a1781abeb1fec905961d80789f5704e71fa05
mersombre <83867104+mersombre@users.noreply.github.com>
2021-08-06 22:00:16 +0900

    Create 5532 방학 숙제.c

---
+++ 5532 방학 숙제.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    int l, a, b, c, d;
+
+    scanf("%d\n%d\n%d\n%d\n%d", &l, &a, &b, &c, &d);
+
+    int num1, num2;
+    int day;
+
+    if (a % c == 0) {
+        num1 = a / c;
+    }
+    else {
+        num1 = a / c + 1;
+    }
+
+    if (b % d == 0) {
+        num2 = b / d;
+    }
+    else {
+        num2 = b / d + 1;
+    }
+
+
+    if (num1 >= num2) {
+        day = l - num1;
+    }
+    else {
+        day = l - num2;
+    }
+
+    printf("%d", day);
+
+    return 0;
+}
